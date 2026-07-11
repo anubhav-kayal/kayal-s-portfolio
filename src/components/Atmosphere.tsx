fa55108bfa0ed2ed8f43e0a750b3e63854d96113
anubhav-kayal <kayalanubhav14@gmail.com>
2026-07-11 12:10:11 +0530

    feat: add layered atmosphere background system

---
+++ src/components/Atmosphere.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function Atmosphere() {
+  return (
+    <div className="app-atmosphere" aria-hidden>
+      <div className="app-atmosphere__wash" />
+      <div className="app-atmosphere__grid" />
+      <div className="app-atmosphere__grid-fine" />
+      <div className="app-atmosphere__noise" />
+      <div className="app-atmosphere__vignette" />
+    </div>
+  );
+}
